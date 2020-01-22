bc5601b7aa88181057e9bbe88cd087dda31be436
Yasir Hassan <13193862+yasirh77@users.noreply.github.com>
2020-01-22 10:46:26 -0800

    Add files via upload
    CIS108 Lab 1

---
+++ Exercise1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Goodbye, cruel world!");
+        }
+    }
+}
